6a4984903ec49250c5e726916afd0041910723ad
sb <chloejjjj@naver.com>
2021-09-22 17:50:29 +0900

    markdown파일 사용하기 위한 라이브러리 셋팅완료

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,10 @@
  */
 
 // You can delete this file if you're not using it
+import 'prismjs/themes/prism-tomorrow.css';
+
+// 그리고 마크다운 내 코드 테마 적용을 위해 gatsby-browser.js 파일에 이렇게 작성
+
+// 여기에서는 prism-tomorrow 테마를 적용해주지만,
+//이 외에도 다양한 테마를 제공하기 때문에 취향에 따라 원하는 테마를 골라서
+//적용해줘도 상관없습니다.
